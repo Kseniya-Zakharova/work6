cb940d886c4f214003172e2db499dfbf0a744d25
Ксения Захарова <shadowkksu87@gmail.com>
2022-07-24 20:30:03 +0300

    Исправление форматирования

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int initialAccount = 100;
-                int depositAmount = 4000;
-                int bonus = 0;
+        int depositAmount = 4000;
+        int bonus = 0;
 
-                if (depositAmount > 1000) {
-                    bonus = depositAmount / 100;}
-                int finalScore = bonus + initialAccount + depositAmount;
+        if (depositAmount > 1000) {
+            bonus = depositAmount / 100;
+        }
+        int finalScore = bonus + initialAccount + depositAmount;
 
-                System.out.println(finalScore);
-                        System.out.println(bonus);
+        System.out.println(finalScore);
+        System.out.println(bonus);
     }
 }
